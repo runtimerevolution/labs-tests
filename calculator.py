ccbc92067f0a4b412354dc5eb77f798236e1d5ef
lmoreira-runtime <165663989+lmoreira-runtime@users.noreply.github.com>
2024-08-08 17:29:27 +0100

    fix

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
+
 class Calculator:
     def add(self, x, y):
         return x + y
-
     def subtract(self, x, y):
         return x - y
+    def hello_world(self):
+        return "Hello World"
--- test_calculator.py
+++ test_calculator.py
@@ -31,6 +31,9 @@ class TestCalculator(unittest.TestCase):
     def test_subtract_zeros(self):
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
+    def test_hello_world(self):
+        self.assertEqual(self.calc.hello_world(), "Hello World")
+
 
 if __name__ == "__main__":
     unittest.main()
