a734af7b5ce10d8e80c72f792be71fcc8f4b6340
lmoreira-runtime <165663989+lmoreira-runtime@users.noreply.github.com>
2024-09-11 16:22:17 +0100

    fix

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
+
 class Calculator:
     def add(self, x, y):
         return x + y
 
     def subtract(self, x, y):
         return x - y
+
+    def hello_world(self):
+        return "Hello World"
