03c1a1c45d6d570a8a878334a996bf09f6d46534
Carlos Martinez <ctmartinez1992@gmail.com>
2024-09-18 16:11:27 +0100

    fix

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
+
 class HelloWorld:
     def print_message(self):
         return "Hello World!"
+
+    def print_modified_message(self):
+        return "Hello, World!"
--- test_hello_world.py
+++ test_hello_world.py
@@ -9,6 +9,9 @@ class TestHelloWorld(unittest.TestCase):
     def test_print_message(self):
         self.assertEqual(self.hw.print_message(), "Hello World!")
 
+    def test_print_modified_message(self):
+        self.assertEqual(self.hw.print_modified_message(), "Hello, World!")
+
 
 if __name__ == "__main__":
     unittest.main()
