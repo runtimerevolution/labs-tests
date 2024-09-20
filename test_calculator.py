2d3f1f34a83ae803b2e19198ef21ba487a4aab22
Carlos Martinez <ctmartinez1992@gmail.com>
2024-09-20 11:25:28 +0100

    fix

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,4 @@
+
 import unittest
 
 from calculator import Calculator
@@ -30,6 +31,31 @@ class TestCalculator(unittest.TestCase):
 
     def test_subtract_zeros(self):
         self.assertEqual(self.calc.subtract(0, 0), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(10, 5), 50)
+
+    def test_multiply_negative_with_positive(self):
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+
+    def test_multiply_negative_with_negative(self):
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+
+    def test_multiply_with_zero(self):
+        self.assertEqual(self.calc.multiply(0, 10), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 5), 2)
+
+    def test_divide_negative_with_positive(self):
+        self.assertEqual(self.calc.divide(-10, 5), -2)
+
+    def test_divide_negative_with_negative(self):
+        self.assertEqual(self.calc.divide(-10, -5), 2)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
 
 
 if __name__ == "__main__":
