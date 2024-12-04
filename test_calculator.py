93e341525c01815b51c5109ee3f21f494f24dff2
jfrverdasca <jfrverdasca@ua.pt>
2024-12-04 15:27:01 +0000

    Fix

--- test_calculator.py
+++ test_calculator.py
@@ -1,30 +1,56 @@
 
 import unittest
-from calculator import add, subtract, multiply, divide
+
+from calculator import Calculator
+
 
 class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(-1, -1), -2)
+        self.assertEqual(self.calc.add(10, 5), 15)
+
+    def test_add_negative_with_positive(self):
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_add_negative_with_negative(self):
+        self.assertEqual(self.calc.add(-1, -1), -2)
+
+    def test_add_zeros(self):
+        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtract(self):
-        self.assertEqual(subtract(10, 5), 5)
-        self.assertEqual(subtract(-1, 1), -2)
-        self.assertEqual(subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+
+    def test_subtract_negative_with_positive(self):
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+
+    def test_subtract_negative_with_negative(self):
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+
+    def test_subtract_zeros(self):
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiply(self):
-        self.assertEqual(multiply(3, 7), 21)
-        self.assertEqual(multiply(-1, 8), -8)
-        self.assertEqual(multiply(-2, -2), 4)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
+
+    def test_multiply_with_zero(self):
+        self.assertEqual(self.calc.multiply(10, 0), 0)
+
+    def test_multiply_negatives(self):
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
 
     def test_divide(self):
-        self.assertEqual(divide(10, 2), 5)
-        self.assertEqual(divide(9, 3), 3)
-        self.assertEqual(divide(-6, 2), -3)
-        with self.assertRaises(ValueError):
-            divide(10, 0)
+        self.assertEqual(self.calc.divide(10, 5), 2)
 
-if __name__ == '__main__':
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
+
+    def test_divide_resulting_in_float(self):
+        self.assertAlmostEqual(self.calc.divide(7, 3), 2.3333333333333335)
+
+
+if __name__ == "__main__":
     unittest.main()
