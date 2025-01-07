e034a1bc8a40245a0f07cc6954cd1178c59d5854
cld-vasconcelos <119345743+cld-vasconcelos@users.noreply.github.com>
2025-01-07 10:32:06 +0000

    Fix

--- calculator.py
+++ calculator.py
@@ -4,3 +4,8 @@ class Calculator:
 
     def subtract(self, x, y):
         return x - y
+
+    def sqrt(self, x):
+        if x < 0:
+            raise ValueError("Cannot compute the square root of a negative number")
+        return x ** 0.5
